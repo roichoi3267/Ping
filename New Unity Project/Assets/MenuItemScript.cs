63749d820697085c974c66b6c5e8c3542353ff96
Roi Choi <roichoi3267@gmail.com>
2020-07-25 16:23:57 +0900

    first commit

---
+++ New Unity Project/Assets/MenuItemScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuItemScript : MonoBehaviour
+{
+    public Color hoverColor;
+    public Color baseColor;
+    public Image background;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background.color = baseColor;
+    }
+
+    // Update is called once per frame
+    public void Select()
+    {
+        background.color = hoverColor;
+    }
+    public void Deselect()
+    {
+        background.color = baseColor;
+    }
+
+    public static implicit operator MenuItemScript(int v)
+    {
+        return new MenuItemScript();
+    }
+}
---
+++ New Unity Project/Assets/MenuScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public Vector2 normalisedMousePosition;
+    public float currentAngle;
+    public int selection;
+    public int previousSelection;
+
+    public GameObject[] menuItems;
+
+    private MenuItemScript menuItemSc;
+    private MenuItemScript previousmenuItemSc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        normalisedMousePosition = new Vector2(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2);
+        currentAngle = Mathf.Atan2(normalisedMousePosition.y, normalisedMousePosition.x) * Mathf.Rad2Deg;
+
+        currentAngle = (currentAngle + 360) % 360;
+
+        selection = (int) currentAngle / 45;
+
+        if (selection != previousSelection)
+        {
+            previousmenuItemSc = menuItems[selection].GetComponent<MenuItemScript>();
+            previousmenuItemSc.Deselect();
+            previousmenuItemSc = selection;
+
+            menuItemSc = menuItems[selection].GetComponent<MenuItemScript>();
+            menuItemSc.Select();
+        }
+
+
+
+        Debug.Log(selection);
+    }
+
+}
+
